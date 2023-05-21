70555b86a5052224289f94760a7549fa7cb19b07
Wisnu Wijaya <61434712+Wisnuoke34@users.noreply.github.com>
2023-05-21 16:41:10 +0700

    Update client_side.js

--- static/js/client_side.js
+++ static/js/client_side.js
@@ -52,10 +52,6 @@ $(document).ready(function(){
       return;
     }
 
-  });
-
-    // -[Prediksi Model]---------------------------
-    
     // Fungsi untuk memanggil API ketika tombol prediksi ditekan
     $("#prediksi_submit").click(function(e) {
       e.preventDefault();
@@ -69,7 +65,7 @@ $(document).ready(function(){
       setTimeout(function() {
         try {
               $.ajax({
-                  url         : "/api/faceDetect",
+                  url         : "/api/deteksi",
                   type        : "POST",
                   data        : pics_data,
                   processData : false,
@@ -94,34 +90,39 @@ $(document).ready(function(){
       }, 1000)  
     })
      
-    // Fungsi untuk menampilkan hasil prediksi analysis pada wajah
+
+  });
+
+    // -[Prediksi Model]---------------------------
+    
+    // Fungsi untuk menampilkan hasil prediksi model
     function generate_prediksi(data_prediksi, image_prediksi) {
       var str="";
       
       if(image_prediksi == "(none)") {
         str += "<h3>Your Image is error</h3>";
-        str += "<img src='https://dummyimage.com/250x250/000/fff' alt='Gambar Produk'>";
+        str += "<img src='https://dummyimage.com/300x300/000/fff' alt='Gambar Produk'>";
       }
       else {
-        str += "<p>Your Problem on Face: <b>"+ data_prediksi +"</b></p>";
-        str += "<img src='" + image_prediksi + "'width=\"300\" height=\"300\" alt='Gambar Produk'>";
+        str += "<p>Your Problem on Face: <b>"+ data_prediksi +"</b></p>" 
+        str += "<img src='" + image_prediksi + "'width=\"250\" height=\"250\" alt='Gambar Produk'>";
       }
       $("#outputAreaFace").html(str);
     }
 
-    // Fungsi untuk menampilkan hasil prediksi analysis pada wajah
-    function generate_recomenn(data_product, image_product) {
+    // Fungsi untuk menampilkan hasil prediksi rekomendasi
+    function generate_recomenn(product_prediction, product_image) {
       var str="";
       
-      if(image_product == "(none)") {
+      if(image_prediksi == "(none)") {
         str += "<h3>Your Image is error</h3>";
-        str += "<img src='https://dummyimage.com/250x250/000/fff' alt='Gambar Produk'>";
+        str += "<img src='https://dummyimage.com/300x300/000/fff' alt='Gambar Produk'>";
       }
       else {
-        str += "<p>Product for Your Face: <b>"+ data_product +"</b></p>";
-        str += "<img src='" + image_prediksi + "'width=\"250\" height=\"250\" alt='Gambar Produk'>";
+        str += "<p>Product on Your Face: <b>"+ product_prediction +"</b></p>" 
+        str += "<img src='" + product_image + "'width=\"250\" height=\"250\" alt='Gambar Produk'>";
       }
       $("#outputAreaFace").html(str);
-    }    
+    }  
   })
   
